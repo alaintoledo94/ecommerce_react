719257f7e5411e4722368c1e61cfa3a0ca05cd0a
AlainToledo <alain.toledo.g@gmail.com>
2023-12-04 21:50:36 -0300

    primmer commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import NavBarComponent from './components/NavBarComponent/NavBarComponent';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+
+
+
+
+
+  return (
+    <div style={{width: '100vw', height: '100vh'}}>
+    <NavBarComponent/>
+    </div>
+  )
+}
+
+export default App
